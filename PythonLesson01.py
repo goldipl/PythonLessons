054c484d69146fc5cbc5d5ca3b886712ca8cbc70
goldipl <mgodlewski92@gmail.com>
2020-11-09 18:42:18 +0100

    1st commit

---
+++ PythonLesson01.py
@@ -0,0 +1,6 @@
+from datetime import date
+print("Today is",date.today().strftime('%A') + ". Year =",date.today().strftime('%Y'))
+print("Hello Python")
+print("Have a nice day!")
+input("Press enter to continue")
+print("Good bye Python")
---
+++ README.md
@@ -0,0 +1,10 @@
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+
+## General info
+My steps of learning Python language.
+
+## Technologies
+Project was created with:
+* Python
