a32e9a0ceeea1a90dc938fb8537e239f94bde7b4
goldipl <mgodlewski92@gmail.com>
2020-11-12 21:59:21 +0100

    Lesson 08: Ternary Operator

---
+++ PythonLesson08.py
@@ -0,0 +1,19 @@
+print("IF-ELSE STATEMENT")
+
+itSnows = True
+
+if itSnows:
+    print("We stay at home")
+else:
+    print("We go out")
+
+print("TERNARY OPERATOR")
+
+print("We stay at home" if itSnows else "We go out")
+
+'''
+IF-ELSE STATEMENT
+We stay at home
+TERNARY OPERATOR
+We stay at home
+'''
